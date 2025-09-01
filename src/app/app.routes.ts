45a3e6f25d61aed403c3a73eb919b4a3425b90b3
Vuk Micic <vukimicic@gmail.com>
2025-09-01 15:53:33 +0200

    -set up main routing in app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'overview',
+    pathMatch: 'full'
+  },
+  {
+    path: 'overview',
+    loadComponent: () => import('./components/overview/overview.component').then(c => c.OverviewComponent)
+  }
+];
